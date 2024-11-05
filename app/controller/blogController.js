e710beac62342dcf465bf7d99e6fe76505bc03df
Md. Rabiul Alam <mavenrabiul@gmail.com>
2024-11-05 16:19:39 +0600

    Update blogController.js
    comment added for deleteBlog

--- app/controller/blogController.js
+++ app/controller/blogController.js
@@ -11,11 +11,13 @@ const readBlog = (req, res) => {
 // Update Route
 const updateBlog = (req, res) => {
     res.status(200).json({ message: "Blog updated successfully" });
+
 };
 
 // Delete Route
 const deleteBlog = (req, res) => {
     res.status(200).json({ message: "Blog deleted successfully" });
+    //code 204 can be use if we don't want return msg.
 };
 
 // Exporting all functions
