08166212af71b6cd08721a857d3e5d2e0486f63a
Md. Rabiul Alam <mavenrabiul@gmail.com>
2024-11-05 16:16:27 +0600

    Update api.js
    api.js is ready for crud operation.

---
+++ routes/api.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { createBlog, readBlog, updateBlog, deleteBlog } from "../app/controller/blogController.js";
+
+const router = express.Router();
+
+router.post("/create-blog", createBlog);
+router.get("/read-blog", readBlog);
+router.put("/update-blog", updateBlog);
+router.delete("/delete-blog", deleteBlog);
+
+export default router;
