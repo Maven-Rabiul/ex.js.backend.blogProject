ac5762a32c842b48f341812e19d19741f22fa607
Md. Rabiul Alam <mavenrabiul@gmail.com>
2024-11-05 16:12:42 +0600

    Update app.js
    app.js file updated

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 
+import express from "express";
+import blogRoutes from "./routes/api.js";
+import SERVER_RUNNING_PORT from "./app/config/config.js";
+
+const app = express();
+
+app.use(express.json()); // Middleware to parse JSON requests
+app.use("/api/blogs", blogRoutes); // Route grouping
+
+app.listen(SERVER_RUNNING_PORT, () => {
+    console.log("Server running on port " + SERVER_RUNNING_PORT + "...");
+});
